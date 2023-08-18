7b8fce56e9407e0d4a0f775c3bc28aadb4e6d0e7
pizzaYami <cyj9407@naver.com>
2023-08-18 11:08:58 +0900

    [feat] 추가 axios설치 및 get요청

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,53 @@
 import data from "../data.js";
 import { Link, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Card() {
+  let [shoes, setShoes] = useState([...data]);
+  let [getCounter, setGetCounter] = useState(2);
+  useEffect(() => {
+    alert("로딩중입니다.");
+  }, [shoes]);
   return (
-    <div className="shoes_container">
-      {data.map((shoes, i) => {
-        return (
-          <div className="shoes_item">
-            {/* eslint-disable-next-line jsx-a11y/alt-text */}
-            <img
-              src={`https://codingapple1.github.io/shop/shoes${i + 1}.jpg`}
-              width="50%"
-            />
-            <h4>{shoes.title}</h4>
-            <p>{shoes.price}</p>
-          </div>
-        );
-      })}
+    <>
+      {getCounter < 4 ? (
+        <button
+          onClick={() => {
+            axios
+              .get(`https://codingapple1.github.io/shop/data${getCounter}.json`)
+              .then((결과) => {
+                let copy = [...shoes, ...결과.data];
+                setShoes(copy);
+                setGetCounter(getCounter + 1);
+              })
+              .catch(() => {
+                console.error("응 실패야~");
+              });
+          }}
+        >
+          서버
+        </button>
+      ) : null}
+
       <Link to="/">홈</Link>
       <Link to="/detail">상세페이지</Link>
-    </div>
+      <div className="shoes_container">
+        {shoes.map((shoes, i) => {
+          return (
+            <div className="shoes_item">
+              {/* eslint-disable-next-line jsx-a11y/alt-text */}
+              <img
+                src={`https://codingapple1.github.io/shop/shoes${i + 1}.jpg`}
+                width="50%"
+              />
+              <h4>{shoes.title}</h4>
+              <p>{shoes.price}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,27 +1,15 @@
-import data from "../data.js";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function Detail({ shoes }) {
   let { id } = useParams();
-  let [num, setNum] = useState("");
-  useEffect(() => {
-    if (isNaN(num) == true) {
-      alert("그러지마세요");
-    }
-  }, [num]);
+
   return (
     <div className="shoes_item">
       {/* eslint-disable-next-line jsx-a11y/alt-text */}
       <img
         src={`https://codingapple1.github.io/shop/shoes${Number(id) + 1}.jpg`}
         width="33%"
-      />
-      <input
-        type="text"
-        onChange={(e) => {
-          setNum(e.target.value);
-        }}
       />
       <h4>{shoes[id].title}</h4>
       <p>{shoes[id].content}</p>
